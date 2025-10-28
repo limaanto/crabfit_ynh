8fe8b86272fa33a96a73bd5ac83cf70420116cf9
Thomas <51749973+Thovi98@users.noreply.github.com>
2025-10-28 15:16:31 +0100

    convert to helpers2.1

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
 #=================================================
-# COMMON VARIABLES
-#=================================================
-
-export nodejs_version="18"
-
-#=================================================
-# PERSONAL HELPERS
+# COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
 
 function build_backend
@@ -16,42 +10,35 @@ function build_backend
 	# Thus the latest version is manually installed alongside the application for the moment
 	pushd $install_dir/api
 		curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > rustup.sh
-		ynh_exec_warn_less ynh_exec_as "$app" \
+		ynh_hide_warnings ynh_exec_as_app \
 			RUSTUP_HOME=$install_dir/api/.rustup \
 			CARGO_HOME=$install_dir/api/.cargo \
 			sh rustup.sh -y -q --no-modify-path --default-toolchain=stable
 		export PATH="$PATH:$install_dir/.cargo/bin"
-		ynh_exec_warn_less ynh_exec_as "$app" \
+		ynh_hide_warnings ynh_exec_as_app \
 			RUSTUP_HOME=$install_dir/api/.rustup \
 			CARGO_HOME=$install_dir/api/.cargo \
 			$install_dir/api/.cargo/bin/cargo update
-		ynh_exec_warn_less ynh_exec_as "$app" \
+		ynh_hide_warnings ynh_exec_as_app \
 			RUSTUP_HOME=$install_dir/api/.rustup \
 			CARGO_HOME=$install_dir/api/.cargo \
 			$install_dir/api/.cargo/bin/cargo build --release --features sql-adaptor
 
 		# Remove build files and rustup
-		ynh_secure_remove --file="$install_dir/api/.cargo"
-		ynh_secure_remove --file="$install_dir/api/.rustup"
+		ynh_safe_rm "$install_dir/api/.cargo"
+		ynh_safe_rm "$install_dir/api/.rustup"
 		mv target/release/crabfit-api ..
-		ynh_secure_remove --file="$install_dir/api/target"
+		ynh_safe_rm "$install_dir/api/target"
 	popd
 }
 
 function build_frontend
 {
+	
 	pushd $install_dir/frontend
-		ynh_exec_warn_less ynh_exec_as "$app" env "$ynh_node_load_PATH" $ynh_npm install next
-		ynh_exec_warn_less ynh_exec_as "$app" env "$ynh_node_load_PATH" $ynh_npm run build
+		ynh_hide_warnings ynh_exec_as_app npm install next
+		ynh_hide_warnings ynh_exec_as_app npm run build
 	popd
 
-	ynh_secure_remove --file="$install_dir/.cache"
+	ynh_safe_rm "$install_dir/.cache"
 }
-
-#=================================================
-# EXPERIMENTAL HELPERS
-#=================================================
-
-#=================================================
-# FUTURE OFFICIAL HELPERS
-#=================================================
